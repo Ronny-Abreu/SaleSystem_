5eeeabbac7c3329ba0bd1bfd022b59c7830f8e6c
tuerre <123309675+Ronny-Abreu@users.noreply.github.com>
2025-07-27 20:23:08 -0400

    CORS FIX

--- backend/index.php
+++ backend/index.php
@@ -1,4 +1,13 @@
 <?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
+
 // Enrutar las requests a la carpeta api
 $request = $_SERVER['REQUEST_URI'];
 $path = parse_url($request, PHP_URL_PATH);
